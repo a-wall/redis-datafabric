e67bec4a5522c686372d00d21038d347b5d9aec7
a-wall <andrewwall@hotmail.com>
2015-03-11 09:54:34 +1300

    Added assembly info.

--- src/Refab/Refab/Properties/AssemblyInfo.cs
+++ src/Refab/Refab/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Refab")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Using Redis as an observable Json data store.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andrew Wall")]
 [assembly: AssemblyProduct("Refab")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
